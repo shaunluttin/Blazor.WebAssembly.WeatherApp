e2278038de00d6a2e06788ad69802524efbe3309
Shaun Luttin <shaun.luttin@mediavalet.com>
2024-06-14 11:09:31 -0700

    Introduce Add, Get, Delete

--- WeatherApp.WebAPI/Program.cs
+++ WeatherApp.WebAPI/Program.cs
@@ -16,29 +16,42 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Add
+app.MapPost("/favorites", () => {
 
-app.MapGet("/weatherforecast", () =>
+})
+.WithName("AddFavorite")
+.WithOpenApi();
+
+// Delete
+app.MapDelete("/favorites", () => {
+
+})
+.WithName("DeleteFavorite")
+.WithOpenApi();
+
+// Get
+app.MapGet("/favorites", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
+        new FavoriteCity
         (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
+            "Vancouver"
         ))
         .ToArray();
+
     return forecast;
 })
-.WithName("GetWeatherForecast")
+.WithName("GetFavorities")
 .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
+
+record FavoriteCity(string CityName)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
